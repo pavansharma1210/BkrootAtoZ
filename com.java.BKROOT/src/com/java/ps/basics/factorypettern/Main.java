cc509e1bf4bf8f80b8ba00fd5fd2fda4ddc80c67
pavansharma1210 <pavansharma02624@gmail.com>
2026-06-09 21:21:40 +0530

    Added Factory Pattern

--- com.java.BKROOT/src/com/java/ps/basics/factorypettern/Main.java
+++ com.java.BKROOT/src/com/java/ps/basics/factorypettern/Main.java
@@ -2,40 +2,43 @@ package com.java.ps.basics.factorypettern;
 
 //Step 1: Create Interface
 interface Payment {
-    void pay();
+    void pay(double amount);
 }
 //Step 2: Implement Classes UPI Payment
 class UpiPayment implements Payment{
     @Override
-    public void pay(){
-        System.out.println("payment through upi");
+    public void pay(double amount){
+        System.out.println("Paid ₹" + amount + " using UPI");
     }
 }
 //Credit Card Payment
 class CreaditCardPayment implements Payment {
     @Override
-    public void pay(){
-        System.out.println("payment throw CreaditCard");
+    public void pay(double amount){
+        System.out.println("Paid ₹" + amount + " using Credit Card");
 
     }
 }
 //paypal Payment
 class PaypalPayment implements Payment{
     @Override
-    public void pay(){
-        System.out.println("payment throw Paypal");
+    public void pay(double amount){
+        System.out.println("Paid ₹" + amount + " using Paypal");
     }
 }
 //Step 3: Create Factory Class
 class PaymentFactory{
-    public static Payment getPayment(String type) {
-        if(type.equalsIgnoreCase("UPI")){
+    public static Payment getPayment(String paymentType) {
+        if (paymentType == null) {
+            return null;
+        }
+        if(paymentType.equalsIgnoreCase("UPI")){
             return new UpiPayment();
         }
-        if(type.equalsIgnoreCase("Card")){
+        if(paymentType.equalsIgnoreCase("Card")){
             return new CreaditCardPayment();
         }
-        if(type.equalsIgnoreCase("PAYPAL")){
+        if(paymentType.equalsIgnoreCase("PAYPAL")){
             return new PaypalPayment();
         }
         return null;
@@ -43,8 +46,12 @@ class PaymentFactory{
 }
 public class Main{
     public static void main(String[] args) {
-        Payment payment = PaymentFactory.getPayment("UPI");
-        payment.pay();
+        Payment payment1 = PaymentFactory.getPayment("UPI");
+        payment1.pay(1000);
+        Payment payment2 = PaymentFactory.getPayment("CARD");
+        payment2.pay(5000);
+        Payment payment3 = PaymentFactory.getPayment("PAYPAL");
+        payment3.pay(3000);
 
     }
 

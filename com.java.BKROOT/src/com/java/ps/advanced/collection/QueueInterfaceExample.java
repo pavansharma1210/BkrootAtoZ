921971e79ee54e49118cc33e9d2aff35afa087dd
pavansharma1210 <pavansharma02624@gmail.com>
2026-06-17 13:25:16 +0530

    save

--- com.java.BKROOT/src/com/java/ps/advanced/collection/QueueInterfaceExample.java
+++ com.java.BKROOT/src/com/java/ps/advanced/collection/QueueInterfaceExample.java
@@ -1,8 +1,25 @@
 package com.java.ps.advanced.collection;
 
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class QueueInterfaceExample {
+    static void priorityQueueExampels(){
+         PriorityQueue<Integer> pq = new PriorityQueue<>(); //min pq
+      //  PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder()); //max pq
+        pq.add(10);
+        pq.add(5);
+        pq.add(7);
+        System.out.println(pq);
+        System.out.println(pq.peek()); // 5- the smallest elmt has the highest priority
+        System.out.println(pq.poll()); // 5 will remove
+        System.out.println(pq);
+        System.out.println(pq.peek());
+
+
+    }
+
     static void QueueExample(){
         // Linked list ko as a queue use kiya h kyoki bo implements karti h queue ko.
 
@@ -21,7 +38,8 @@ public class QueueInterfaceExample {
 
     }
     public static void main(String[] args) {
-        QueueExample();
+        priorityQueueExampels();
+       // QueueExample();
 
 
     }

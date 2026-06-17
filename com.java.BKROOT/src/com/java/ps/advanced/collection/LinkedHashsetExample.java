1157632e9ca01c36ca7ba99fbc0a15eeaa1d8e27
pavansharma1210 <pavansharma02624@gmail.com>
2026-06-17 16:25:38 +0530

    save

---
+++ com.java.BKROOT/src/com/java/ps/advanced/collection/LinkedHashsetExample.java
@@ -0,0 +1,17 @@
+package com.java.ps.advanced.collection;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashsetExample {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> st = new LinkedHashSet<>();
+        st.add(1);
+        st.add(2);
+        st.add(3);
+        st.add(48);
+        st.add(15);
+        st.add(10);
+        System.out.println(st);
+
+    }
+}
--- com.java.BKROOT/src/com/java/ps/advanced/collection/SetInterfaceExamples.java
+++ com.java.BKROOT/src/com/java/ps/advanced/collection/SetInterfaceExamples.java
@@ -1,13 +1,17 @@
 package com.java.ps.advanced.collection;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class SetInterfaceExamples {
-    public static void main(String[] args) {
+    static void Hashsetexample(){
         HashSet<Integer> st = new HashSet<>();
         st.add(1);
         st.add(2);
         st.add(3);
+        st.add(48);
+        st.add(15);
+        st.add(10);
         System.out.println(st); // 1 2 3
         st.add(1);// store only unique value , dublicate not allow
         st.add(1);
@@ -16,7 +20,21 @@ public class SetInterfaceExamples {
         st.remove(2);
         System.out.println(st);
         System.out.println(st.contains(2)); // false
-        System.out.println(st.size()); //2
+        System.out.println(st.size()); //5
+    }
+    static void LinkedHashsetExample(){ //order of insertion preserve ho jata h
+        LinkedHashSet<Integer> st = new LinkedHashSet<>();
+        st.add(1);
+        st.add(2);
+        st.add(3);
+        st.add(48);
+        st.add(15);
+        st.add(10);
+        System.out.println(st);
+    }
+    public static void main(String[] args) {
+        //Hashsetexample();
+        LinkedHashsetExample();
 
     }
 }

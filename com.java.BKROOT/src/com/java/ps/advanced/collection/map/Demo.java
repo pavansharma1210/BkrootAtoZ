3f6f927fbb6008164c1e0c935fbb29f8d471ded4
pavansharma1210 <pavansharma02624@gmail.com>
2026-06-17 17:24:50 +0530

    save

---
+++ com.java.BKROOT/src/com/java/ps/advanced/collection/map/Demo.java
@@ -0,0 +1,14 @@
+package com.java.ps.advanced.collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Demo {
+    public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap<>();
+        map.put(101, " Pavan");
+        map.put(102, " Rahul");
+        System.out.println(map);
+
+    }
+}
